04a382292293122c27abeb51d3c36b68bf33265e
Amit Daya <amitdaya@in-di-go.com>
2017-01-30 23:27:45 -0500

    feat: some more homepage enhancements...

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Footer } from 'react-mdl';
-import Image from '../../components/Image/Image';
+
+import {
+  Grid, Cell, Card, CardActions, CardTitle, Icon, Button, FABButton
+} from 'react-mdl';
+
 import {
   ParallaxContainer, ParallaxGroup, ParallaxLayerFore, ParallaxLayerBase, ParallaxLayerBack, ParallaxLayerDeep
 } from '../../components/Parallax/Parallax';
@@ -8,45 +11,111 @@ import {
 const Home = () => (
   <ParallaxContainer className="icg-home-container">
 
-    <ParallaxGroup className="icg-home-intro">
-      <ParallaxLayerFore className="title">
-        <h2>Welcome to Indigo Consulting Group</h2>
-      </ParallaxLayerFore>
+    <div className="icg-home-intro">
+      <ParallaxGroup>
+        <ParallaxLayerFore className="title">
+          <h2>Welcome to Indigo Consulting Group</h2>
+        </ParallaxLayerFore>
 
-      <ParallaxLayerBase>
-        <h3>
-          Design
-          <span className="spacer"></span>
-          User Experience
-          <span className="spacer"></span>
-          Usability
-        </h3>
-      </ParallaxLayerBase>
+        <ParallaxLayerBase>
+          <h3>
+            Design
+            <div className="spacer"> </div>
+            User Experience
+            <div className="spacer"> </div>
+            Usability
+          </h3>
+        </ParallaxLayerBase>
 
-      <ParallaxLayerBack>
-        <h4>Enriching Lives Through Positive User Experiences</h4>
-      </ParallaxLayerBack>
-    </ParallaxGroup>
+        <ParallaxLayerBack>
+          <h4>Enriching Lives Through Positive User Experiences</h4>
+        </ParallaxLayerBack>
+      </ParallaxGroup>
 
-    <ParallaxGroup className="icg-home-mission">
-      <ParallaxLayerBase>
-        <h2>Mission</h2>
-      </ParallaxLayerBase>
+      <FABButton colored ripple>
+          <Icon name="add" />
+      </FABButton>
+    </div>
+    <div className="icg-home-mission">
+      <ParallaxGroup>
+        <ParallaxLayerBase>
+          <h2>Mission</h2>
+        </ParallaxLayerBase>
 
-      <ParallaxLayerBack>
-        <h3>Blah Blah</h3>
-      </ParallaxLayerBack>
-    </ParallaxGroup>
+        <ParallaxLayerBack>
+          <h3>Blah Blah</h3>
+        </ParallaxLayerBack>
+      </ParallaxGroup>
+    </div>
 
-    <ParallaxGroup className="icg-home-services">
-      <ParallaxLayerBase>
-        <h2>Services</h2>
-      </ParallaxLayerBase>
+    <div className="icg-home-mission">
+      <ParallaxGroup className="icg-home-services">
+        <ParallaxLayerBase>
+          <h2>Services</h2>
+        </ParallaxLayerBase>
 
-      <ParallaxLayerBack>
-        <h3>Blah Blah</h3>
-      </ParallaxLayerBack>
-    </ParallaxGroup>
+        <ParallaxLayerBack>
+
+          <Grid style={{padding: '0 2em'}}>
+            <Cell col={4} tablet={12} phone={12}>
+
+              <Card shadow={0} className="card-full-width">
+                  <CardTitle expand style={{alignItems: 'flex-start', color: '#333'}}>
+                      <h4 style={{marginTop: '0'}}>
+                          Featured event:<br />
+                          May 24, 2016<br />
+                          7-11pm
+                      </h4>
+                  </CardTitle>
+                  <CardActions border style={{borderColor: 'rgba(255, 255, 255, 0.2)', display: 'flex', boxSizing: 'border-box', alignItems: 'center', color: '#333'}}>
+                      <Button colored style={{color: '#fff'}}>Add to Calendar</Button>
+                      <div className="mdl-layout-spacer"></div>
+                      <Icon name="event" />
+                  </CardActions>
+              </Card>
+
+            </Cell>
+            <Cell col={4} tablet={12} phone={12}>
+
+              <Card shadow={0} className="card-full-width">
+                  <CardTitle expand style={{alignItems: 'flex-start', color: '#333'}}>
+                      <h4 style={{marginTop: '0'}}>
+                          Featured event:<br />
+                          May 24, 2016<br />
+                          7-11pm
+                      </h4>
+                  </CardTitle>
+                  <CardActions border style={{borderColor: 'rgba(255, 255, 255, 0.2)', display: 'flex', boxSizing: 'border-box', alignItems: 'center', color: '#333'}}>
+                      <Button colored style={{color: '#fff'}}>Add to Calendar</Button>
+                      <div className="mdl-layout-spacer"></div>
+                      <Icon name="event" />
+                  </CardActions>
+              </Card>
+
+            </Cell>
+            <Cell col={4} tablet={12} phone={12}>
+
+              <Card shadow={0} className="card-full-width">
+                  <CardTitle expand style={{alignItems: 'flex-start', color: '#333'}}>
+                      <h4 style={{marginTop: '0'}}>
+                          Featured event:<br />
+                          May 24, 2016<br />
+                          7-11pm
+                      </h4>
+                  </CardTitle>
+                  <CardActions border style={{borderColor: 'rgba(255, 255, 255, 0.2)', display: 'flex', boxSizing: 'border-box', alignItems: 'center', color: '#333'}}>
+                      <Button colored style={{color: '#fff'}}>Add to Calendar</Button>
+                      <div className="mdl-layout-spacer"></div>
+                      <Icon name="event" />
+                  </CardActions>
+              </Card>
+
+            </Cell>
+          </Grid>
+
+        </ParallaxLayerBack>
+      </ParallaxGroup>
+    </div>
 
   </ParallaxContainer>
 );
